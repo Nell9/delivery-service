c2fa0a2a57e0580a11df8ce206a4169ba7701d53
Рабочий пк <gmail.com>
2025-02-18 17:17:28 +0300

    ready

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import sys
 
+# Решается методом двух указателей
+# Сортируем список по убыванию
+# Смотрим, если data[0] > option -> error
+# Иначе выкидываем все data[i] == option
+# И методом двух указателей проходимся по списку
+
 
 def search_min_platforms_count(data: list, option: int):
-    ...
+    sort_data = sorted(data, reversed = True)
+    result_count = 0
+    if sort_data[0] > option:
+        return "error"
+    for i in range(sort_data):
+        if sort_data[i] == option:
+            result_count += 1
+            sort_data[i] = [None]
+    i = 0
+    j = len(sort_data-1)
+    while i < j:
+        if sort_data[i] + sort_data[j] == option:
+            result_count += 1
+			j-=1
+        elif sort_data[i] + sort_data[j] > option:
+            i += 1
+        elif sort_data[i] + sort_data[j] < option:
+            j -= 1
 
 
 def get() -> tuple[list, int]:
@@ -11,4 +34,4 @@ def get() -> tuple[list, int]:
     return data, option
 
 
-data, option = get()
+search_min_platforms_count(*get())
