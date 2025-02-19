0c3875f368856a73b41e5640a01cf69dfd1165da
Рабочий пк <gmail.com>
2025-02-19 09:51:33 +0300

    Правки ревью2

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
-# id: 133632349
+# id: 133641462
 
 
-def get_min_platforms_count(robots_weight: tuple[int], limit: int) -> int:
+from typing import Iterable
+
+
+def get_min_platforms_count(
+        robots_weights: Iterable[int], 
+        limit: int
+        ) -> int:
     """
     Получает значение минимального количества платформ для перевозки роботов.
         Параметры:
         robots_weight: tuple[int] - содержит веса роботов.
         limit: int - предельный вес платформы.
     """
-    weights: list = sorted(robots_weight)
+    weights = sorted(robots_weights)
     platforms_count = 0
-    min_element = 0
-    max_element = len(weights)-1
+    min_index = 0
+    max_index = len(weights)-1
 
-    while min_element <= max_element:
-        if (weights[min_element] + weights[max_element] <= limit):
-            min_element += 1
-        max_element -= 1
+    while min_index <= max_index:
+        if weights[min_index] + weights[max_index] <= limit:
+            min_index += 1
+        max_index -= 1
         platforms_count += 1
     return platforms_count
 
 
 if __name__ == '__main__':
-
-    def get() -> tuple[tuple[int], int]:
-        robots_weight = map(int, input().split(' '))
-        limit = int(input())
-        return tuple(robots_weight), limit
-
-    print(get_min_platforms_count(*get()))
+    robots_weights = map(int, input().split(' '))
+    limit = int(input())
+    print(get_min_platforms_count(robots_weights, limit))
